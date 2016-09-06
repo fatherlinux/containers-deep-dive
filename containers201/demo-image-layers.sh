3382fc467da29f27ef8feaef15dcd5d464bb4f53
root <root@rhel7.dc2.crunchtools.com>
2016-09-06 04:15:13 -0400

    SM: updated demos

--- containers201/demo-image-layers.sh
+++ containers201/demo-image-layers.sh
@@ -1,1 +1,1 @@
-docker run --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock nate/dockviz images --tree rhel7
+docker run --rm --privileged -v /var/run/docker.sock:/var/run/docker.sock nate/dockviz images --tree registry.access.redhat.com/rhel7
--- containers201/demo-repositories.sh
+++ containers201/demo-repositories.sh
@@ -1,9 +1,9 @@
-docker pull rhel7
-echo ""
-docker pull rhel7/rhel
-echo ""
 docker pull registry.access.redhat.com/rhel7
 echo ""
 docker pull registry.access.redhat.com/rhel7/rhel
+echo ""
+docker tag registry.access.redhat.com/rhel7/rhel rhel7
+echo ""
+docker tag registry.access.redhat.com/rhel7/rhel rhel7/rhel
 echo ""
 docker images | grep rhel7
