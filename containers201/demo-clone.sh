30f953b9ba85823712d5b37d4703c797f1e1ee61
root <root@rhel7.dc2.crunchtools.com>
2016-10-04 00:08:17 -0400

    SM: updated for better clone/execve demo

--- containers201/demo-clone.sh
+++ containers201/demo-clone.sh
@@ -1,3 +1,19 @@
+#!/bin/bash
+#
 # Developed with these instructions
 # https://access.redhat.com/documentation/en/red-hat-enterprise-linux-atomic-host/7/paged/getting-started-with-containers/chapter-15-finding-and-running-containers-without-docker-runc-and-skopeo
-strace runc start rhenl-container 2>&1 | grep clone
+
+# Run the container detached from the terminal
+# Had to disable the terminal in config.json ("terminal": false)
+strace -o strace.txt runc start -d rhel-container
+
+# Show the system call, clean up the directory
+cat strace.txt | grep clone
+rm -f strace.txt
+
+# Show the process table
+sleep 3
+ps -efZ | grep sleep
+
+# Delete the container
+runc delete rhel-container
--- containers201/demo-execve.sh
+++ containers201/demo-execve.sh
@@ -1,2 +1,3 @@
-strace sleep 3 2>&1 | grep exec &
+strace sleep 10 2>&1 | grep exec &
+sleep 3
 ps -efZ | grep sleep
